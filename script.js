6927ada1ca8a98df597452f215adfb0a09751f0c
nhat2017 <118781353+nhat2017@users.noreply.github.com>
2022-12-03 16:20:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function myFunction() {
-  alert("I glad you tell me that");
-}
+
+var btn = document.getElementById(‘btn');
+btn.addEventListener(‘click', function(e) {
+ e.preventDefaul()
+ var mame = document .getElementById( ‘name’ ).value;
+var mame = document .getElementById( ‘email’ ).value;
+var mame = document .getElementById( ‘major’ ).value;
+var mame = document .getElementById( ‘message’ ).value;
+ 
+
+var body = ‘name: ' +name + ‘<br/> email:' <br/> subject’ + subject  +'<br/>message' + message;
+
+
+
+
+
+Email.send({
+    Host : "smtp.gmail.com",
+    Username : "thespycia2015@gmail.com,
+    Password : "agwqvfmubupqfqhk",
+    To : 'thespycia2015@gmail.com',
+    From : "you@isp.com",
+    Subject : "This is the major",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
